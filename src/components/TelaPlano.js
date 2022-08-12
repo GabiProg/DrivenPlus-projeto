3b283d66de5349c4b9cbf5f72c943b063a4d7c4d
GabiProg <gabrielaterese07@gmail.com>
2022-08-12 15:49:51 -0300

    Resolução TelaPlano

--- src/components/TelaPlano.js
+++ src/components/TelaPlano.js
@@ -1,13 +1,367 @@
 import styled from "styled-components";
+import Imgae from "../assets/Group 1.png";
+import { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+import axios from "axios";
 
-export default function TelaPlano(){
-    return(
-    <Conteiner>
-        Tela Plano
-    </Conteiner>
-    );
+export default function TelaPlano() {
+    const [click, setClick] = useState(false);
+    const [plano, setPlano] = useState();
+    const {token} = useContext(UserContext);
+    const {ID_DO_PLANO} = useParams();
+
+    useEffect(() => {
+        const URL = `https://mock-api.driven.com.br/api/v4/driven-plus/subscriptions/memberships/${ID_DO_PLANO}`;
+
+        const config = {
+            headers: {
+              "Authorization": `Bearer ${token}`
+            }
+        }
+
+        const promise = axios.get(URL, config);
+        
+        promise.then((res) => {
+            setPlano(res.data);
+            console.log(res.data);
+        });
+
+    }, []);
+
+    console.log(plano);
+    
+
+    function TelaPlanoItens(props){
+    
+        const { price, image, name} = props.plano;
+       
+        console.log(price, image, name, props);
+    
+        return(
+        <>
+            <img src={image} alt="Imagem do plano escolhido" />
+            <Titulo>{name}</Titulo>
+            <Beneficios>
+              <div>
+                <ion-icon name="clipboard-outline"></ion-icon>
+                <p>Benefícios:</p>
+              </div>
+              <Pacote>
+                {plano?.perks.map((item, index) => {
+                    const {id, title} = item;
+                    return <PacoteItens key={id} id={id} title={title} index={index}/>})}
+              </Pacote>
+            </Beneficios>
+            <Preco>
+              <div>
+                <ion-icon name="cash-outline"></ion-icon>
+                <p>Preco:</p>
+              </div>
+              <span>R$ {price} cobrados mensalmente</span>
+            </Preco>
+        </>
+        );
+    }
+
+    function PacoteItens({id, title, index}){
+        return(
+            <>
+            <h3>1.Brindes exclusivos</h3>
+            </>
+        );
+    }
+
+  return (
+    <>
+      <Conteiner>
+        <ion-icon name="arrow-back-outline"></ion-icon>
+        <TelaPlanoItens plano={plano}/>
+        <Formulario>
+          <input type="text" placeholder="Nome impresso no cartão" />
+          <input type="text" placeholder="Digitos do cartão" />
+          <div>
+            <input id="first" type="text" placeholder="Código de segurança" />
+            <input id="second" type="text" placeholder="Validade" />
+          </div>
+          <button onClick={() => setClick(true)}>ASSINAR</button>
+        </Formulario>
+      {click === true ? (<Modal>
+        <div>
+          <ion-icon name="close-outline" onClick={() => setClick(false)}></ion-icon>
+        </div>
+        <Quadro>
+          <p>
+            Tem certeza que deseja
+            <br /> assinar o plano
+            <br /> Driven Plus (R$ 39,99)?
+          </p>
+          <Confirmar>
+            <Nao onClick={() => setClick(false)}>Não</Nao>
+            <Sim>SIM</Sim>
+          </Confirmar>
+        </Quadro>
+      </Modal>): <div></div>}
+        </Conteiner>
+    </>
+  );
 }
 
 const Conteiner = styled.div`
-    
+  width: 375px;
+  height: auto;
+  background: #0e0e13;
+  box-sizing: border-box;
+  position: relative;
+
+  ion-icon {
+    margin-top: 24.35px;
+    margin-left: 22px;
+    margin-bottom: 35.35px;
+    font-size: 28px;
+    color: #ffffff;
+  }
+
+  img {
+    margin-left: 100px;
+    margin-right: 129.62px;
+    margin-bottom: 11.87px;
+  }
+`;
+
+const Titulo = styled.div`
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 38px;
+  color: #ffffff;
+  margin-left: 100px;
+  margin-right: 100.62px;
+  margin-bottom: 22px;
+`;
+
+const Beneficios = styled.div`
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 230px;
+  }
+
+  div p {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #ffffff;
+    margin-bottom: 12px;
+    margin-top: 10px;
+  }
+
+  ion-icon {
+    font-size: 24px;
+    color: #ff4791;
+    margin-bottom: 12px;
+    margin-top: 10px;
+  }
+`;
+
+const Preco = styled.div`
+  margin-bottom: 30px;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 260px;
+  }
+
+  p {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #ffffff;
+    margin-bottom: 4px;
+    margin-top: 4px;
+  }
+
+  ion-icon {
+    color: #ff4791;
+    font-size: 24px;
+    margin-right: 3px;
+    margin-bottom: 4px;
+    margin-top: 4px;
+  }
+
+  span {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+    margin-left: 40px;
+  }
+`;
+const Formulario = styled.div`
+  div {
+    display: flex;
+  }
+  input {
+    height: 52px;
+    width: 299px;
+    border-radius: 8px;
+    background: #ffffff;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: #7e7e7e;
+    margin: 0 36px 8px 40px;
+    padding-left: 10px;
+    box-sizing: border-box;
+  }
+  div input {
+    height: 52px;
+    width: 145px;
+    border-radius: 8px;
+  }
+
+  button {
+    width: 299px;
+    height: 52px;
+    background: #ff4791;
+    border-radius: 8px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+    margin-bottom: 34px;
+    margin-right: 36px;
+    margin-left: 40px;
+  }
+
+  #first {
+    margin-right: 7px;
+  }
+  #second {
+    margin: 0 56px 0 0;
+  }
+`;
+
+const Pacote = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 299px;
+  height: auto;
+
+  h3 {
+    margin-right: 100px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+  }
+  h4 {
+    margin-right: 65px;
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: #ffffff;
+  }
+`;
+
+const Modal = styled.div`
+  width: 375px;
+  height: 667px;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  div ion-icon {
+    margin-left: 327px;
+    margin-bottom: 300px;
+    color: #ffffff;
+    font-size: 40px;
+  }
+`;
+
+const Quadro = styled.div`
+  width: 248px;
+  height: 210px;
+  background: #ffffff;
+  border-radius: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 100px;
+
+  p {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+    color: #000000;
+  }
+`;
+
+const Confirmar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 47px;
+`;
+
+const Nao = styled.div`
+  width: 95px;
+  height: 52px;
+  background: #cecece;
+  border-radius: 8px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 16px;
+  color: #ffffff;
+  margin-left: 22px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Sim = styled.div`
+  width: 95px;
+  height: 52px;
+  background: #ff4791;
+  border-radius: 8px;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  color: #ffffff;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
